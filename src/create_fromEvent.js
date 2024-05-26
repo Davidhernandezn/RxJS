070c47ca20a0aec96ee418601118937c3250bf7b
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-05-26 14:37:35 -0600

    FROM EVENT reaccionar a eventos de la interfaz

---
+++ src/create_fromEvent.js
@@ -0,0 +1,28 @@
+import { fromEvent } from 'rxjs';
+import { displayLog } from './utils';
+
+/**
+*OBSERVABLE QUE ASOCIADO A UN EVENT TARGET, PARA EMITIR LOS EVENTOS GENERADOS
+*CANALIZA EVENTOS DEL DOM 
+*O EVENT EMITERS DE NODE
+*REACCIONAR A EVENTOS DE LA INTERFAZ 
+*/
+
+export default () => {
+//REFERENCIAMOS ELEMENTO
+    const actionBtn = document.getElementById('action-btn');
+
+//observable apartir de los eventos (target y eventoAEscuchar);
+const source = fromEvent(actionBtn, 'click');
+
+//NOS SUSCRIBIMOS AL OBSERVABLE A LOS DATOS (DATOS RECIBIDOS)
+//LO QUE HARÉ CON LOS DATOS
+source.subscribe(evt => {
+    displayLog(`click event at pos (${evt.x}, ${evt.y})`);
+})
+
+
+//NUEVO
+fromEvent(document, 'mousemove').subscribe(evt =>{
+    console.log(evt);
+})} 
